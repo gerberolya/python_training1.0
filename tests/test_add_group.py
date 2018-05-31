3f2683916cf4038fed75ae02dfb73ab1b9cdc142
Olya <gerberolya@gmail.com>
2018-05-31 11:49:09 +0300

    Создание пакетов для разных зон ответственности

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from group import Group
-from application import Application
+from model.group import Group
+from Fixture.application import Application
 import pytest
 
 @pytest.fixture
