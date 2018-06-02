1d1866675848fa992548ea1bb571d5c3d6e33de3
Olya <gerberolya@gmail.com>
2018-06-02 14:54:25 +0300

    Выделен класс GroupHelper

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -13,13 +13,13 @@ def app(request):
 def test_add_group(app):
     success = True
     app.session.login(username="admin", password="secret")
-    app.create_new_group(Group(name="test_name", header="test_header", footer="test_footer"))
+    app.group.create(Group(name="test_name", header="test_header", footer="test_footer"))
     app.session.logout()
 
 def test_add_empty_group(app):
     success = True
     app.session.login(username="admin", password="secret")
-    app.create_new_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
 
