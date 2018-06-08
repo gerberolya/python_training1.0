2190c9d4aa4932dc629b303797a1d357a5eae5fb
Olya <gerberolya@gmail.com>
2018-06-08 14:29:54 +0300

    Начало 8 задания

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,8 +1,14 @@
 from model.group import Group
 
 
-def test_modify_first_group(app):
+def test_modify_name(app):
     success = True
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Group(name="modify_name", header="modify_header", footer="modify_footer"))
+    app.group.modify_first_group(Group(name="modify_name"))
+    app.session.logout()
+
+def test_modify_header(app):
+    success = True
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="modify_nheader"))
     app.session.logout()
