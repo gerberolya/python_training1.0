e1c59bc94f5e4468a493c33558e319d34fb78200
Olya <gerberolya@gmail.com>
2018-06-11 23:44:57 +0300

    Создан тест модификации групп. Логин и логаут перенесены в conftest

--- conftest.py
+++ conftest.py
@@ -2,8 +2,12 @@ from fixture.application import Application
 import pytest
 
 
-@pytest.fixture (scope = 'session')
+@pytest.fixture(scope = 'session')
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
+    fixture.session.login(username="admin", password="secret")
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
     return fixture
