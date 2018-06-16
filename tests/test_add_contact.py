b213d9807a0e500eb4382d5140eca6b318fcf809
Olga Gerber <gerberolya@gmail.com>
2018-06-16 12:44:27 +0300

    Добавлено сравнение списков групп после добавления, удаления  или модифицирования контакта

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -4,7 +4,10 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.fill_in_the_form(Contact(firstname="Olga", lastname="Tester", company="test_company", address="test_address", email="test123@test123.com"))
+    contact_info = Contact(firstname="Olga", lastname="Tester", company="test_company", address="test_address", email="test123@test123.com")
+    app.contact.fill_in_the_form(contact_info)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact_info)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
--- tests/test_modify_contact.py
+++ tests/test_modify_contact.py
@@ -1,10 +1,14 @@
 from model.contact import Contact
 
 
-def test_modify_contact_name(app):
+def test_modify_contact_lastname(app):
     if app.contact.count() == 0:
-        app.contact.fill_in_the_form(Contact(firstname="for modify"))
+        app.contact.fill_in_the_form(Contact(lastname="for modify"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(firstname="Olga2"))
+    contact_info = Contact(lastname="Tester2")
+    contact_info.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact_info)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact_info
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
