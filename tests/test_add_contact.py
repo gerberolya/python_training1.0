6c30bc8dc0c762df02ebe753d71bb5c746e89e16
Olga Gerber <gerberolya@gmail.com>
2018-06-22 15:17:02 +0300

    Параметизованы тесты для создания групп и контактов

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,13 +1,30 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import string
+import random
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [
+    Contact(firstname=random_string("name", 10), lastname=random_string("surname", 10),
+            company=random_string("company", 10), address=random_string("address", 10))
+    for firstname in ["", random_string("name", 10)]
+    for lastname in ["", random_string("surname", 10)]
+    for company in ["", random_string("company", 10)]
+    for address in ["", random_string("address", 10)]
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact_info = Contact(firstname="Olga", lastname="Tester", company="test_company", address="test_address", email="test123@test123.com")
-    app.contact.create(contact_info)
+    app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    old_contacts.append(contact_info)
+    old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,24 +1,31 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import string
+import random
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [
+    Group(name=name, header=header, footer=footer)
+    for name in ["", random_string("name", 10)]
+    for header in ["", random_string("header", 10)]
+    for footer in ["", random_string("footer", 10)]
+]
 
 
-def test_add_group(app):
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
+    pass
     old_groups = app.group.get_group_list()
-    group_info = Group(name="test_name", header="test_header", footer="test_footer")
-    app.group.create(group_info)
+    app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
-    old_groups.append(group_info)
+    old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-#def test_add_empty_group(app):
-#    old_groups = app.group.get_group_list()
-#    group_info = Group(name="", header="", footer="")
-#    app.group.create(group_info)
-#    new_groups = app.group.get_group_list()
-#    assert len(old_groups) + 1 == len(new_groups)
-#    old_groups.append(group_info)
-#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
