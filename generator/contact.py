7f9ee0eb53266812492ba7b5e4343703ff091217
Olga Gerber <gerberolya@gmail.com>
2018-07-06 11:26:19 +0300

    Загрузка тестовых данных из файла json (using jsonpickle), корректировка тестов контактов

--- generator/contact.py
+++ generator/contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 import string
 import random
 import os.path
-import json
+import jsonpickle
 import sys
 import getopt
 
@@ -41,4 +41,5 @@ testdata = [Contact(firstname="", lastname="", company="", address="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
