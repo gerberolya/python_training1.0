a79c829171a2b3a84b73b321999ae9b709d6cc36
Olga Gerber <gerberolya@gmail.com>
2018-07-09 14:53:00 +0300

    7урок 4 лекция

--- fixture/db.py
+++ fixture/db.py
@@ -1,6 +1,8 @@
 __author__ = 'tester'
 
 import pymysql.cursors
+from model.group import Group
+
 
 class DbFixture:
 
@@ -10,6 +12,19 @@ class DbFixture:
         self.user = user
         self.password = password
         self.connection = pymysql.connect(host=host, database=database, user=user, password=password)
+        self.connection.autocommit = True
+
+    def get_group_list(self):
+        group_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return group_list
 
     def destroy(self):
         self.connection.close()
