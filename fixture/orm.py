d26a105c9527808bd4883960cb01579b82e86e28
Olga Gerber <gerberolya@gmail.com>
2018-07-12 12:58:33 +0300

    Create requests using ORM

--- fixture/orm.py
+++ fixture/orm.py
@@ -1,4 +1,8 @@
 from pony.orm import *
+from datetime import datetime
+from model.group import Group
+from model.contact import Contact
+from pymysql.converters import decoders
 
 
 class ORMFixture:
@@ -18,3 +22,27 @@ class ORMFixture:
         firstname = Optional(str, column="firstname")
         lastname = Optional(str, column="lastname")
         deprecated = Optional(datetime, column="deprecated")
+
+    def __init__(self, host, database, user, password):
+        self.db.bind('mysql', host="127.0.0.1", database="addressbook", user="root", password="", conv=decoders)
+        self.db.generate_mapping()
+
+
+    def convert_groups_to_model(self, groups):
+        def convert(group):
+            return Group(id=str(group.id), name=group.name, header=group.header, footer=group.footer)
+        return list(map(convert, groups))
+
+
+    @db_session
+    def get_group_list(self):
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+
+    def convert_contacts_to_model(self, contacts):
+        def convert(contact):
+            return Contact(id=str(contact.id), firstname=contact.firstname, lastname=contact.lastname)
+        return list(map(convert, contacts))
+
+    @db_session
+    def get_contact_list(self):
+        return self.convert_contacts_to_model(select(c for c in ORMFixture.ORMContact if c.deprecated is None))
