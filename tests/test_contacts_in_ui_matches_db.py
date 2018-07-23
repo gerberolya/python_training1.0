7def2bc95453a1027c4b26002fb4479dee36b4f8
Olga Gerber <gerberolya@gmail.com>
2018-07-23 13:49:50 +0300

    Tests_failed

--- tests/test_contacts_in_ui_matches_db.py
+++ tests/test_contacts_in_ui_matches_db.py
@@ -12,11 +12,11 @@ def test_contacts_on_main_page_and_db_the_same(app, db):
     contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
     assert len(contacts_from_home_page) == len(contacts_from_db)
     for num in range(len(contacts_from_db)):
-        assert contacts_from_home_page[num].lastname == contacts_from_db[num].lastname
-        assert contacts_from_home_page[num].firstname == clear(contacts_from_db[num].firstname)
-        assert contacts_from_home_page[num].address == clear(contacts_from_db[num].address)
-        assert contacts_from_home_page[num].all_emails_from_home_page == merge_emails_like_on_home_page(contacts_from_db[num])
-        assert contacts_from_home_page[num].all_phones_from_home_page == merge_phones_like_on_home_page(contacts_from_db[num])
+        assert clear(contacts_from_home_page[num].lastname) == clear(contacts_from_db[num].lastname)
+        assert clear(contacts_from_home_page[num].firstname) == clear(contacts_from_db[num].firstname)
+        assert clear(contacts_from_home_page[num].address) == clear(contacts_from_db[num].address)
+        assert clear(contacts_from_home_page[num].all_emails_from_home_page) == clear(merge_emails_like_on_home_page(contacts_from_db[num]))
+        assert clear(contacts_from_home_page[num].all_phones_from_home_page) == clear(merge_phones_like_on_home_page(contacts_from_db[num]))
 
 
 def merge_emails_like_on_home_page(contact):
